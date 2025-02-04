fa10c1c67becf5c22113b90cedafb7a8a7a3707c
Amy Nguyen <amynguyen@Brents-MacBook-Pro.local>
2025-02-03 20:25:25 -0800

    Updates

---
+++ precache-manifest.0873211e73f338ef08d386db610287a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9393c5381c2df77bf105dc2b5cdc9576",
+    "url": "/brent-valentine/index.html"
+  },
+  {
+    "revision": "c07fbcd2d0c76ae3f9fe",
+    "url": "/brent-valentine/static/css/main.6ccd7dcc.chunk.css"
+  },
+  {
+    "revision": "49ec81ddbc4040de18da",
+    "url": "/brent-valentine/static/js/2.7e445aac.chunk.js"
+  },
+  {
+    "revision": "79e6e8bcdf4a5810f4c0",
+    "url": "/brent-valentine/static/js/3.3667de86.chunk.js"
+  },
+  {
+    "revision": "c07fbcd2d0c76ae3f9fe",
+    "url": "/brent-valentine/static/js/main.1c5ab70f.chunk.js"
+  },
+  {
+    "revision": "370a9d20d8dc41df51a2",
+    "url": "/brent-valentine/static/js/runtime~main.6427c02e.js"
+  }
+]);
